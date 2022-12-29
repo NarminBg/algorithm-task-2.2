334d792d5a279488864417209cf80302d2f1b3cf
[Narmin Bagirova] <[narminnbagirova@gmail.com]>
2022-12-29 19:04:18 +0400

    Add project files.

---
+++ algorithm task 2.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace algorithm_task_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int count = 0;
+            int character;
+            while (number!=0)
+            {
+                character = number % 10;
+                number /= 10;
+
+                if (character==0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+           
+        }
+    }
+}
